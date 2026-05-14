1f3bcffea5723ae04325bdc8e8e2090129c43d0d
Nitik Banwala <nitikbanwal@Nitiks-MacBook-Air.local>
2026-05-14 15:32:52 +0530

    fix image

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import Footer from "@/components/common/Footer";
 export const metadata = {
   title: "PEPE-HUNT",
   description: "PEPE-HUNT",
+  images: ["public/meta.png"],
   openGraph: {
     title: "PEPE-HUNT",
     description: "PEPE-HUNT",
